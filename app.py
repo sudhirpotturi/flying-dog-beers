5008568766f1ecc1b181ae37ef557d9b600f0bca
sudhirpotturi <50867186+sudhirpotturi@users.noreply.github.com>
2019-08-20 21:33:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-# from iexfinance.stocks import get_historical_data
-# from iexfinance.stocks import Stock
 from dash.dependencies import Input,Output
-import datetime
-from  dateutil.relativedelta import relativedelta
 import pandas as pd
 import plotly.graph_objs as go
-start=datetime.datetime.today()-relativedelta(years=5)
-end=datetime.datetime.today()
-
 
 app=dash.Dash()
-#
-# app.layout=html.Div([
-#     html.Div([dcc.Input(id="stock-input",value="SPY",type="text")]),
-#     html.H1(children="Stock Chart"),
-#
-# html.Div([
-#         html.Div([
-#             dcc.Graph(
-#                 id="graph_close",
-#             )
-#
-# ])
-# ])
-# ])
 
 app.layout = html.Div([
     dcc.Dropdown(
@@ -45,36 +24,18 @@ app.layout = html.Div([
                 )
     ])
 ])
-    ])
-
-
-
-# app.css.append_css({
-#     "external_url":"https://contextsummits.com/wp-content/themes/contextsummits/css/fx3.css"
-# }
-
+])
 
 @app.callback(dash.dependencies.Output("graph_close","figure"),
               [dash.dependencies.Input("stock-input","value")]
               )
 def update_fig(input_value):
-    # df = get_historical_data(input_value, start=start, end=end, output_format='pandas',
-    #                           token='pk_15ba07e215294ebda89c0d0950f0fdba')
     df=pd.read_csv(r"SP500.csv");
     data=[]
-    # trace_close = go.Scatter(x=list(df.index), y=list(df.close))
     if input_value=="SP":
         trace_close = go.Scatter(x=list(df.Month), y=list(df.SP))
     else:
         trace_close = go.Scatter(x=list(df.Month), y=list(df.Fund))
-    # df = df.sort_values(['SP', 'Fund'], ascending=[True, True])
-    # df=df.head(10)
-    # trace_close = go.Figure(data=[
-    #     go.Bar(name='SP', x=df.Month, y=df.SP),
-    #     go.Bar(name='OldOrchard', x=df.Month, y=df.Fund)
-    # ])
-    # # Change the bar mode
-    # trace_close.update_layout(barmode='group')
     data.append(trace_close)
     layout={"title":input_value}
     return{
@@ -83,5 +44,3 @@ def update_fig(input_value):
       }
 if __name__=="__main__":
     app.run_server(debug=True)
-
-
