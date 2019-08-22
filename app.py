0bf1d9e00d490167ee82179254a907e1cb01573d
sudhirpotturi <50867186+sudhirpotturi@users.noreply.github.com>
2019-08-22 21:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,53 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 import pandas as pd
-import plotly.plotly as py
-
-py.sign_in("sudhirjntu", "Srikar*9")
-
-
-app = dash.Dash(__name__)
-server=app.server
-
 
 data=pd.read_csv(r"SP500.csv");
 df = data.sort_values(['SP', 'Fund'], ascending=[True, True])
 df=df.head(10)
 
-df1=pd.read_csv(r"VAMI1.csv");
-df1=df1.sample(10)
-df1 = df1.sort_values(['VAMI'], ascending=[True])
+########### Set up the chart
+beers=df['Month']
+ibu_values=df['SP']
+abv_values=df['Fund']
+color1='lightblue'
+color2='darkgreen'
 
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name='S&P',
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name='Fund',
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = 'Beer Comparison'
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+########### Display the chart
+
+app = dash.Dash()
+server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Tear sheet Visualization'),
-
-
+    html.H1('Context'),
     dcc.Graph(
-        id='graph1',
-        figure={
-            'data': [
-                {'x': df['Month'], 'y': df['SP'], 'type': 'bar', 'name': 'SP'},
-                {'x': df['Month'], 'y': df['Fund'] , 'type': 'bar', 'name': u'Fund'},
-            ],
-            'layout': {
-                'title': 'PERFORMANCE DURING WORST 10 MONTHS OF S&P 500 PERFORMANCE'
-            }
-        }
-    ),
-
-    dcc.Graph(
-        id='graph2',
-        figure={
-            'data': [
-                {'x': data['SP'], 'y': data['Fund'], 'type': 'scatter', 'name': 'SP','mode': 'markers'}
-            ],
-            'layout': {
-                'title': 'FUND RETURNS VS SPX RETURNS'
-            }
-        }
-    ),
-    dcc.Graph(
-        id='graph3',
-        figure={
-            'data': [
-                {'x': data['Fund'], 'type': 'histogram', 'name': 'SP', 'xbins': {'end':50, 'size':5, 'start': -50}}
-            ],
-            'layout': {
-                'title': 'Distribution of Returns'
-            }
-        }
-    ),
-    dcc.Graph(
-        id='graph4',
-        figure={
-            'data': [
-                {'x': df1['Month'], 'y': df1['VAMI'], 'type': 'scatter', 'name': 'SP','mode': 'markers+lines'}
-            ],
-            'layout': {
-                'title': 'VAMI',
-                'yaxis': dict(range=[0, 4000])
-            }
-        }
-    )
-
-])
+        id='flyingdog',
+        figure=beer_fig
+    )    ]
+)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
