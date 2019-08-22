344dc7fa6ae1aceca064cc92e14779be7aab4e66
sudhirpotturi <50867186+sudhirpotturi@users.noreply.github.com>
2019-08-22 19:03:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,76 @@
+# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input,Output
 import pandas as pd
-import plotly.graph_objs as go
 
-app=dash.Dash()
-server = app.server
 
-app.layout = html.Div([
-    dcc.Dropdown(
-        id='stock-input',
-        options=[
-            {'label': 'S&P500', 'value': 'SP'},
-            {'label': 'Old Orchard', 'value': 'Old Orchard'},
+app = dash.Dash(__name__)
+server=app.server()
 
-        ],
-        value='SP'
+
+data=pd.read_csv(r"SP500.csv");
+df = data.sort_values(['SP', 'Fund'], ascending=[True, True])
+df=df.head(10)
+
+df1=pd.read_csv(r"VAMI1.csv");
+df1=df1.sample(10)
+df1 = df1.sort_values(['VAMI'], ascending=[True])
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Tear sheet Visualization'),
+
+
+    dcc.Graph(
+        id='graph1',
+        figure={
+            'data': [
+                {'x': df['Month'], 'y': df['SP'], 'type': 'bar', 'name': 'SP'},
+                {'x': df['Month'], 'y': df['Fund'] , 'type': 'bar', 'name': u'Fund'},
+            ],
+            'layout': {
+                'title': 'PERFORMANCE DURING WORST 10 MONTHS OF S&P 500 PERFORMANCE'
+            }
+        }
     ),
-    html.Div([
-            html.Div([
-                dcc.Graph(
-                    id="graph_close",
-                )
-    ])
-])
+
+    dcc.Graph(
+        id='graph2',
+        figure={
+            'data': [
+                {'x': data['SP'], 'y': data['Fund'], 'type': 'scatter', 'name': 'SP','mode': 'markers'}
+            ],
+            'layout': {
+                'title': 'FUND RETURNS VS SPX RETURNS'
+            }
+        }
+    ),
+    dcc.Graph(
+        id='graph3',
+        figure={
+            'data': [
+                {'x': data['Fund'], 'type': 'histogram', 'name': 'SP', 'xbins': {'end':50, 'size':5, 'start': -50}}
+            ],
+            'layout': {
+                'title': 'Distribution of Returns'
+            }
+        }
+    ),
+    dcc.Graph(
+        id='graph4',
+        figure={
+            'data': [
+                {'x': df1['Month'], 'y': df1['VAMI'], 'type': 'scatter', 'name': 'SP','mode': 'markers+lines'}
+            ],
+            'layout': {
+                'title': 'VAMI',
+                'yaxis': dict(range=[0, 4000])
+            }
+        }
+    )
+
 ])
 
-@app.callback(dash.dependencies.Output("graph_close","figure"),
-              [dash.dependencies.Input("stock-input","value")]
-              )
-def update_fig(input_value):
-    df=pd.read_csv(r"SP500.csv");
-    data=[]
-    if input_value=="SP":
-        trace_close = go.Scatter(x=list(df.Month), y=list(df.SP))
-    else:
-        trace_close = go.Scatter(x=list(df.Month), y=list(df.Fund))
-    data.append(trace_close)
-    layout={"title":input_value}
-    return{
-        "data":data,
-        "layout":layout
-      }
-if __name__=="__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)
